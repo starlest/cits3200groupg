d6f9fe25f4528e9cd2b64ee37d38c2fd2c48e74a
Edwin Chia <starlest@gmail.com>
2014-10-14 22:29:41 +0800

    Revert "test"
    This reverts commit e79374998c3da9d14af51926025aca8b8054765f.

---
+++ GuildWayfinding/src/com/example/guildwayfinding/AppIntentService.java
@@ -0,0 +1,105 @@
+package com.example.guildwayfinding;
+
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class AppIntentService extends IntentService {
+	private ServerSocket server;
+
+  /**
+   * A constructor is required, and must call the super IntentService(String)
+   * constructor with a name for the worker thread.
+   */
+  public AppIntentService() {
+      super("AppIntentService");
+  }
+
+  /**
+   * The IntentService calls this method from the default worker thread with
+   * the intent that started the service. When this method returns, IntentService
+   * stops the service, as appropriate.
+   */
+  @Override
+  protected void onHandleIntent(Intent intent) {
+		Log.i("AppIntentService", "Started");
+		start();
+  }
+  
+  public void start() {
+	try {
+		//Socket socket = server.accept();
+		Socket socket = new Socket("192.168.1.29",8000);
+		
+		
+		PrintWriter outputStream = new PrintWriter(socket.getOutputStream());
+		BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	
+		Log.i("AppIntentService", "Connected");
+		
+		DBHelper db = new DBHelper(this);
+	
+		
+		while(true){
+			String action = inputStream.readLine();
+			String[] split = action.split("---");
+			String method = split[0];
+			
+	
+			//Handle request
+			if (method.equals("LIST")) {		
+				Log.i("AppIntentService", "List Command Received");
+				List<String> list = db.getStaffsIdsNames();
+				Iterator<String> it = list.iterator();
+				
+				while(it.hasNext()){
+					String next = it.next();
+					outputStream.println(next);
+					//Log.i("AppIntentService", next);
+				}
+				
+				outputStream.println("/DONE");
+				outputStream.flush();
+				
+				if (outputStream.checkError())
+					Log.i("AppIntentService", "Error");
+	
+			} else if(method.equals("DETAILS")){
+				Log.i("AppIntentService", "Details Command Received");
+				String id = split[1];
+				Staff s = db.getStaff(Integer.parseInt(id));
+				
+				outputStream.println(id+"&"+s.getName()+"&"+s.getRoom()+"&"+s.getFaculty()+"&"+s.getTelephone()+"&"+s.getEmail()+"&"+s.mon+"&"+s.tues+"&"+s.wed+"&"+s.thurs+"&"+s.fri);
+			
+				outputStream.flush();
+	
+	
+			} else if (method.equals("EDIT")){
+				Log.i("AppIntentService", "Edit Command Received");
+				String[] parts = split[1].split("&");
+				db.editStaff(Integer.parseInt(parts[0]),parts[1],parts[2],parts[3],parts[4],parts[5],parts[6],parts[7],parts[8],parts[9],parts[10]);
+				
+			} else if (method.equals("ADD")){
+				
+				Log.i("AppIntentService", "Add Command Received");
+				String[] parts = split[1].split("&");
+				db.addStaff(parts[0],Integer.parseInt(parts[1]),parts[2],parts[3],parts[4],parts[5],parts[6],parts[7],parts[8],parts[9]);
+			}
+		}
+	} catch (Exception e) {
+		Log.i("AppIntentService", "Error connecting to Server, Retrying in 10secs");
+		
+		//retry after 10 secs
+		new Timer().schedule(new TimerTask() {          
+		    @Override
+		    public void run() {
+		        start();      
+		    }
+		}, 10000);
+	}
+  }
+}
